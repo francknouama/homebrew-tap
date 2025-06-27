0e57a0a08aa15141da7cbd95b0ac8c3ab21c22ff
Franck NOUAMA <586511+francknouama@users.noreply.github.com>
2025-06-27 10:39:37 -0400

    Update go-starter.rb revert to 1.2.1

--- Formula/go-starter.rb
+++ Formula/go-starter.rb
@@ -1,7 +1,7 @@
 class GoStarter < Formula
   desc "Comprehensive Go project generator with modern best practices"
   homepage "https://github.com/francknouama/go-starter"
-  url "https://github.com/francknouama/go-starter/archive/v1.3.0.tar.gz"
+  url "https://github.com/francknouama/go-starter/archive/v1.2.1.tar.gz"
   sha256 "33f5df4abbb80db4fcf8ed677b802a554f7406842ae76cca2190ec1201575303"
   license "MIT"
 
